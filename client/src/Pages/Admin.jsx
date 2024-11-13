fafd4225d91bbf3671f31be08e7590d519506754
salamus <salamus@yandex.com>
2024-11-13 19:01:59 +0300

    end 1.5

--- client/src/Pages/Admin.jsx
+++ client/src/Pages/Admin.jsx
@@ -65,7 +65,7 @@ function Admin() {
       setJsonText(JSON.stringify(result, null, 2));
   
       // Отправка данных на сервер
-      fetch('http://90.156.169.196:4444/data', {
+      fetch('http://90.156.169.196:4444/update-data', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
